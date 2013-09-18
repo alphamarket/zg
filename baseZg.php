463c0cb60a605c5130a35d842a1424d975134315
dariush <b.g.dariush@gmail.com>
2013-09-18 08:32:12 +0430

    ~ bin/zg + baseZg.php + zg.php

---
+++ baseZg.php
@@ -0,0 +1,13 @@
+<?php
+namespace zinux\zg;
+require_once dirname(__FILE__)."/../baseZinux.php";
+
+/**
+ * Description of baseZg
+ *
+ * @author dariush
+ */
+abstract class baseZg extends \zinux\baseZinux
+{
+    public function Initiate(){}
+}
---
+++ zg.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'baseZg.php';
+/**
+ * Description of zg
+ *
+ * @author dariush
+ */
+class zg extends zinux\zg\baseZg
+{
+}
