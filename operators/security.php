66a61b4ae87cbd35c4dae2fc3f42ed9d6effa601
dariush <b.g.dariush@gmail.com>
2013-09-23 08:36:36 +0330

    basic security added

---
+++ operators/security.php
@@ -0,0 +1,19 @@
+<?php
+namespace zinux\zg\operators;
+
+/**
+ * Description of security
+ *
+ * @author dariush
+ */
+class security extends baseOperator
+{
+    public function encrypt($args)
+    {
+        $this->cout(__METHOD__);
+    }
+    public function decrypt($args)
+    {
+        $this->cout(__METHOD__);  
+    }
+}
