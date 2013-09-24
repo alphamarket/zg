1f220ce5167e47757699eefe038fcead0ed9429c
dariush <b.g.dariush@gmail.com>
2013-09-24 11:01:45 +0330

    vendor/creator.php is done docing

--- vendor/creator.php
+++ vendor/creator.php
@@ -1,33 +1,47 @@
 <?php
 namespace zinux\zg\vendor;
 /**
- * Description of createmoduleBootstrap
- *
- * @author dariush
+ * Zinux item creation handler
  */
 class creator extends \zinux\zg\operators\baseOperator
 {
+    /**
+     * ctor a new creator
+     */
     public function __construct()
     {
+        # suppress header text
         parent::__construct(1);
     }
+    /**
+     * Handlers module creatation
+     * @param string $name the module's name
+     * @param string $projectDir project directory to create
+     * @return \zinux\zg\vendor\item the created module
+     * @throws \zinux\kernel\exceptions\invalideOperationException in case of module's folder already exists
+     */
     public function createModule($name ,$projectDir = ".")
     {
+        # this opt is valid under project directories
         $this->CheckZG($projectDir,1);
+        # get status object
         $s = $this->GetStatus($projectDir);
-        
+        # check if module folder does not exist
         if(!file_exists($s->modules->meta->path))
             mkdir($s->modules->meta->path, 0775);
+        # normalizing the args
         $name = preg_replace("#(\w+)module$#i","$1", $name)."Module";
         $bs_name = preg_replace("#(\w+)module$#i","$1", $name)."Bootstrap";
-        
+        # in case of module folder already exist
         if(\zinux\kernel\utilities\fileSystem::resolve_path("{$s->modules->meta->path}/{$name}"))
             throw new \zinux\kernel\exceptions\invalideOperationException("Module '{$name}' already exists ...");
-            
+        # create a module item
         $module = new \zinux\zg\vendor\item("{$name}", "{$s->modules->meta->path}/{$name}", $s->modules->meta);
+        # add the module to modules collections
         $s->modules->collection[strtolower($module->name)] = $module;
+        # save status object
         $this->SaveStatus($s);
-        
+        # create directory structures
         $this->Run(array(
                 "mkdir {$module->path}",
                 "cd {$module->path} && mkdir controllers",
@@ -38,82 +52,174 @@ class creator extends \zinux\zg\operators\baseOperator
                 "cd {$module->path}/views && mkdir layout",
                 "chmod 775 -R {$module->path}"    
         ));
+        # add a new bootstrap to module
         new \zinux\zg\vendor\creators\moduleBootstrap($module, new \zinux\zg\vendor\Item("{$bs_name}", $module->path."/{$bs_name}.php"), $projectDir);
+        # return created module
         return $module;
     } 
+    /**
+     * creates new controller
+     * @param string $name controller's name 
+     * @param \zinux\zg\vendor\Item $module parent module object
+     * @param string $projectDir project direcroty
+     * @return \zinux\zg\vendor\Item created controller
+     */
     public function createController($name, Item $module ,$projectDir = ".")
     {
+        # this opt is valid under project directories
         $this->CheckZG($projectDir,1);
-        $s = $this->GetStatus($projectDir);
+        # normalizing the arg
         $name = preg_replace("#(\w+)controller$#i","$1", $name)."Controller";
+        # create new controller object
         $controller = new \zinux\zg\vendor\Item($name, $module->path."/controllers/{$name}.php");
+        # create the controller
         new \zinux\zg\vendor\creators\createController($module, $controller, $projectDir);
+        # return created controller
         return $controller;
     }
+    /**
+     * creates new action
+     * @param string $name action's name
+     * @param \zinux\zg\vendor\item $controller parent controller object
+     * @param string $projectDir project directory
+     * @return \zinux\zg\vendor\Item created action
+     */
     public function createAction($name, item $controller,$projectDir = ".")
     {
+        # this opt is valid under project directories
         $this->CheckZG($projectDir,1);
-        $s = $this->GetStatus($projectDir);
+        # normalizing the arg
         $name = preg_replace("#(\w+)action$#i","$1", $name)."Action";
-        return new \zinux\zg\vendor\creators\createAction(
-                $controller, 
-                new \zinux\zg\vendor\item($name, $name));
+        # create the action object
+        $action =  new \zinux\zg\vendor\item($name, $name);
+        # create the action
+        new \zinux\zg\vendor\creators\createAction($controller, $action);
+        # return created action
+        return $action;
     }
     
+    /**
+     * creates new application bootstrap
+     * @param string $name bootstrap's name
+     * @param string $projectDir project directory
+     * @return \zinux\zg\vendor\Item created bootstrap
+     */
     public function createAppBootstrap($name, $projectDir = ".")
     {
+        # this opt is valid under project directories
         $this->CheckZG($projectDir,1);
+        # get status object
         $s = $this->GetStatus($projectDir);
+        # normalizing the arg
         $name = preg_replace("#(\w+)bootstrap$#i","$1", $name)."Bootstrap";
+        # create the bs object
         $appbs = new \zinux\zg\vendor\Item($name, $s->project->path."/application/{$name}.php");
+        # create the bs
         new \zinux\zg\vendor\creators\appBootstrap($s->project, $appbs, $projectDir);
+        # return the created bs
         return $appbs;
     }
+    /**
+     * creates new application routes
+     * @param string $name routes' name
+     * @param string $projectDir project directory
+     * @return \zinux\zg\vendor\Item
+     */
     public function createAppRoutes($name, $projectDir = ".")
     {
+        # this opt is valid under project directories
         $this->CheckZG($projectDir,1);
+        # get status object
         $s = $this->GetStatus($projectDir);
+        # normalizing the arg
         $name = preg_replace("#(\w+)routes$#i","$1", $name)."Routes";
+        # create routes object
         $appr = new \zinux\zg\vendor\Item($name, $s->project->path."/application/{$name}.php");
+        # create the routes
         new \zinux\zg\vendor\creators\appRoutes($s->project, $appr, $projectDir);
+        # return the created routes
         return $appr;
     }
+    /**
+     * creates new view 
+     * @param string $name view's name
+     * @param \zinux\zg\vendor\item $controller parent controller
+     * @param string $projectDir project directory
+     * @return \zinux\zg\vendor\Item created view
+     */
     public function createView($name, item $controller, $projectDir = ".")
     {
+        # this opt is valid under project directories
         $this->CheckZG($projectDir,1);
-        $s = $this->GetStatus($projectDir);
+        # normalizing the arg
         $name = preg_replace("#(\w+)view$#i","$1", $name)."View";
+        # create view object
         $view = new \zinux\zg\vendor\Item($name, 
             $controller->parent->path."/views/view/".preg_replace("#(\w+)controller$#i","$1", basename($controller->path, ".php"))."/{$name}.phtml");
+        # create the view
         new \zinux\zg\vendor\creators\createView($controller, $view, $projectDir);
+        # return the created view
         return $view;
     }
+    /**
+     * creates new layout
+     * @param string $name layout's name
+     * @param \zinux\zg\vendor\Item $module parent module
+     * @param string $projectDir project directory
+     * @return \zinux\zg\vendor\Item created layout
+     */
     public function createLayout($name, Item $module ,$projectDir = ".")
     {
+        # this opt is valid under project directories
         $this->CheckZG($projectDir,1);
-        $s = $this->GetStatus($projectDir);
+        # normalizing the arg
         $name = preg_replace("#(\w+)layout$#i","$1", $name)."Layout";
-        $controller = new \zinux\zg\vendor\Item($name, $module->path."/views/layout/{$name}.phtml");
-        new \zinux\zg\vendor\creators\createLayout($module, $controller, $projectDir);
-        return $controller;
+        # create new layout object
+        $layout = new \zinux\zg\vendor\Item($name, $module->path."/views/layout/{$name}.phtml");
+        # create the layout
+        new \zinux\zg\vendor\creators\createLayout($module, $layout, $projectDir);
+        # return created layout
+        return $layout;
     }
+    /**
+     * creates new helper
+     * @param string $name helper's name
+     * @param \zinux\zg\vendor\Item $module parent module
+     * @param string $projectDir project directory
+     * @return \zinux\zg\vendor\Item created helper
+     */
     public function createHelper($name, Item $module ,$projectDir = ".")
     {
+        # this opt is valid under project directories
         $this->CheckZG($projectDir,1);
-        $s = $this->GetStatus($projectDir);
+        # normalizing the arg
         $name = preg_replace("#(\w+)helper$#i","$1", $name)."Helper";
+        # create helper object
         $helper = new \zinux\zg\vendor\Item($name, $module->path."/views/helper/{$name}.php");
+        # create the helper
         new \zinux\zg\vendor\creators\createHelper($module, $helper, $projectDir);
+        # return created helper
         return $helper;
     }
+    /**
+     * creates new model
+     * @param string $name model's name
+     * @param \zinux\zg\vendor\Item $module parent module
+     * @param string $projectDir project directory
+     * @return \zinux\zg\vendor\Item created model
+     */
     public function createModel($name, Item $module ,$projectDir = ".")
     {
+        # this opt is valid under project directories
         $this->CheckZG($projectDir,1);
-        $s = $this->GetStatus($projectDir);
+        # normalizing the arg
         # no naming convention for models
         # $name = preg_replace("#(\w+)helper$#i","$1", $name)."Helper";
+        # create model object
         $model = new \zinux\zg\vendor\Item($name, $module->path."/models/{$name}.php");
+        # create the model
         new \zinux\zg\vendor\creators\createModel($module, $model, $projectDir);
+        # return created model
         return $model;
     }
     
