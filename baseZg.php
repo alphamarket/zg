a0b121f2c5963c22d231fe820f0e3e08f5e28146
dariush <b.g.dariush@gmail.com>
2013-09-24 16:30:58 +0330

    1.4.31 is ready

--- baseZg.php
+++ baseZg.php
@@ -21,7 +21,7 @@ if(!defined("ZG_ROOT"))
     # defines default command files' root
     defined("COMMANDS_ROOT") || define("COMMANDS_ROOT", ZG_ROOT.'/resources/commands');
     # defines ZG's version
-    defined("ZG_VERSION") || define("ZG_VERSION","1.4.30");
+    defined("ZG_VERSION") || define("ZG_VERSION","1.4.31");
     # defines running environment
     defined("RUNNING_ENV") || define("RUNNING_ENV","PRODUCTION");
     # an other alternative running environment definition
