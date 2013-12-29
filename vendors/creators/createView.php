e5c3e2e37552abad5db6c4965872cfbdd82a0230
dariush <b.g.dariush@gmail.com>
2013-12-29 11:08:47 +0330

    @ vendors/creators/createView.php: improved

--- vendors/creators/createView.php
+++ vendors/creators/createView.php
@@ -13,6 +13,7 @@ class createView extends \zinux\zg\baseZg
      */
     public function __construct(\zinux\zg\vendors\item $controller, \zinux\zg\vendors\item $view, $project_path = ".")
     {
+        $view_path = \preg_replace("#//#i", "/", $view->path);
         $ns = $this->convert_to_relative_path($view->path, $project_path);;
         $this ->cout("Creating new view '", 0.5,  self::getColor(self::defColor), 0)
                 ->cout($view->name, 0, self::getColor(self::yellow), 0)
@@ -21,17 +22,17 @@ class createView extends \zinux\zg\baseZg
                 ->cout("' controller.");
         if(!\zinux\kernel\utilities\fileSystem::resolve_path(dirname($view->path)))
             mkdir(dirname($view->path), 0775);
-        
+        $view->action = preg_replace("#(.*)(view)$#i", "$1Action", $view->name);
+        $view->extention = preg_replace("#(.*)\.(.*)$#i","$2",$view->path);
         $mbc = "<!--
- The $ns\\{$view->name}
+ The \\$ns\\{$view->name}
  @by Zinux Generator <b.g.dariush@gmail.com>
  -->
 <p>
-    A view for <b>{$controller->name}</b>.<br />
-    Location '<b>{$view->path}</b>'.
+    A view for '<b>\\{$this->convert_to_relative_path($controller->path, $project_path)}\\{$controller->name}::{$view->action}()</b>'<br />
+    Location: '<b>{$view_path}</b>'
 </p>";
         file_put_contents($view->path, $mbc);
-        
         $s = $this->GetStatus($project_path);
         $view->parent = $controller;
         $s->modules->collection[strtolower($controller->parent->name)]->controller[strtolower($controller->name)]->view[strtolower($view->name)] = $view;
